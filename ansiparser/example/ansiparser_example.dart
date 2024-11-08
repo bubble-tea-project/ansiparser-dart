d14b7012bc2eb0a214a1d1c2c4bca3939bb5c46d
vHrqO <120627599+vHrqO@users.noreply.github.com>
2024-11-09 02:41:35 +0800

    Update example

--- ansiparser/example/ansiparser_example.dart
+++ ansiparser/example/ansiparser_example.dart
@@ -1,5 +1,11 @@
-import 'package:ansiparser/ansiparser.dart';
+import 'package:ansiparser/ansiparser.dart' as ansiparser;
 
 void main() {
-  
+  final ansipScreen = ansiparser.newScreen();
+  ansipScreen.put('\x1b[1;6H-World!\x1b[1;1HHello');
+
+  ansipScreen.parse();
+  final converted = ansipScreen.toFormattedString();
+
+  print(converted); // ['Hello-World!']
 }
