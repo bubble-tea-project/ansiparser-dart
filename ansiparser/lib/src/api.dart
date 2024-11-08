8c5c663f4aabf8b7b3fff538b7332dee289a5877
vHrqO <120627599+vHrqO@users.noreply.github.com>
2024-11-09 00:55:28 +0800

    refactor

--- ansiparser/lib/src/api.dart
+++ ansiparser/lib/src/api.dart
@@ -1,22 +1,21 @@
-/*
-ansiparser.api
-~~~~~~~~~~~~~~
-
-This module implements the ansiparser API.
-*/
+/// ansiparser.api
+/// ----------
+///
+/// This module implements the ansiparser API.
+library;
 
 import './screen_parser.dart';
 import './structures.dart';
 
-
-// Initialize the ScreenParser for a new screen.
+/// Initialize the ScreenParser for a new screen.
 ScreenParser newScreen({int height = 24, int width = 80}) {
   return ScreenParser(screenHeight: height, screenWidth: width);
 }
 
-// Initialize the ScreenParser from an existing parsed screen.
+/// Initialize the ScreenParser from an existing parsed screen.
 ScreenParser fromScreen(List<InterConverted> parsedScreen) {
   var screenParser = ScreenParser();
   screenParser.fromParsedScreen(parsedScreen);
+
   return screenParser;
 }
