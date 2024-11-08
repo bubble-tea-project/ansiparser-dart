d03abc0206da47f63e41d3dc23baa35b086683ba
vHrqO <120627599+vHrqO@users.noreply.github.com>
2024-11-08 22:11:54 +0800

    refactor

--- ansiparser/lib/src/re_pattern.dart
+++ ansiparser/lib/src/re_pattern.dart
@@ -1,28 +1,26 @@
-/*
-ansiparser.re_pattern
-~~~~~~~~~~~~
+/// ansiparser.re_pattern
+/// ----------
+///
+/// This module implements commonly used regular expression patterns for ansiparser.
+library;
 
-This module implements commonly used regular expression patterns for ansiparser.
-*/
-
-
-// ANSI escape sequences
+/// ANSI escape sequences
 final RegExp ansiEscape = RegExp(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])');
 
-// CSI (Control Sequence Introducer) sequences
+/// CSI (Control Sequence Introducer) sequences
 final RegExp csiSequence = RegExp(r'\x1B(?:\[[0-?]*[ -/]*[@-~])');
 
-// SGR (Select Graphic Rendition)
+/// SGR (Select Graphic Rendition)
 final RegExp sgrSequence = RegExp(r'\x1B(?:\[([\d;]*)m)');
 
-// Erase in Display
+/// Erase in Display
 final RegExp eraseDisplaySequence = RegExp(r'\x1B(?:\[([\d;]*)J)');
 
-// Erase in Display - clear entire screen
+/// Erase in Display - clear entire screen
 final RegExp eraseDisplayClearScreen = RegExp(r'\x1B(?:\[2J)');
 
-// Erase in Line
+/// Erase in Line
 final RegExp eraseLineSequence = RegExp(r'\x1B(?:\[([\d;]*)K)');
 
-// Cursor Position
+/// Cursor Position
 final RegExp cursorPositionSequence = RegExp(r'\x1B(?:\[([\d;]*)H)');
